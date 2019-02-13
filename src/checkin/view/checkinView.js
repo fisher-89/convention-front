07df174e91102a3c9bdfbe12bf99f4bb866165ad
sshch3612 <1758974924@qq.com>
2019-02-13 15:33:03 +0800

    签到授权

--- src/checkin/view/checkinView.js
+++ src/checkin/view/checkinView.js
@@ -8,9 +8,9 @@ export default class CheckIn extends React.Component {
    componentWillMount(){
    }
    componentDidMount(){
-     const url = encodeURIComponent('http://192.168.1.95:8000/checkin/checkin.html');
+     const url = encodeURIComponent('http://cs.xigemall.com/checkin/checkin.html');
      const appId = 'wx136539e52b4980bf';
-     window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=SCOPE&state=STATE#wechat_redirect`
+     window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`
    }
    render(){
      return <div>
