c690f275fb43b2b8d4488733b783acdb4eab8cc0
sshch3612 <1758974924@qq.com>
2019-02-13 16:48:47 +0800

    const url = encodeURIComponent(http://cs.xigemall.com/checkin/checkin.html);

--- src/checkin/view/checkinView.js
+++ src/checkin/view/checkinView.js
@@ -9,9 +9,13 @@ export default class CheckIn extends React.Component {
    }
    componentDidMount(){
     console.log(window.location.href); 
-    const url = encodeURIComponent('http://cs.xigemall.com/checkin/checkin.html');
-    const appId = 'wx136539e52b4980bf';
-    window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`
+    const openId = sessionStorage.getItem('check_openid');
+    if(!openId){
+      const url = encodeURIComponent('http://cs.xigemall.com/checkin/checkin.html');
+      const appId = 'wx136539e52b4980bf';
+      window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`;
+      sessionStorage.setItem('check_openid','123');
+    }
    }
    render(){
      return <div>
