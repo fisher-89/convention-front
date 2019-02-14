0bd70aa9fa7065d4fd4c72cd615f1042b363e563
sshch3612 <1758974924@qq.com>
2019-02-14 17:41:45 +0800

    修改webpack

--- src/checkin/view/checkinView.js
+++ src/checkin/view/checkinView.js
@@ -23,7 +23,7 @@ export default class CheckIn extends React.Component {
       if(!code){
         const url = encodeURIComponent('http://cs.xigemall.com/checkin/checkin.html');//
         const appId = 'wx136539e52b4980bf';
-        window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`;
+        // window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`;
         // sessionStorage.setItem('check_openid','123');//
       }else{
         const data = {'code':code}
@@ -48,6 +48,10 @@ export default class CheckIn extends React.Component {
     this.state.code = code;
   }   
    render(){
+    const {code } = this.state;
+    // if(!code){
+    //   return <Loading/>
+    // }
      return(<React.Fragment>
         <Suspense fallback={<Loading/>}>
           <CheckForm/>
--- src/checkin/view/checksub.js
+++ src/checkin/view/checksub.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Toast} from 'antd-mobile';
+import axios from 'axios';
 import './checkform.less';
 import './checksub.less';
 import logo from 'public/checkin/logo.png';
@@ -8,7 +10,47 @@ import gxline from 'public/checkin/gxline.png';
 import triangle from 'public/checkin/triangle.png';
 import smline from 'public/checkin/smline.png';
 
+
 export default class CheckSub extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      acountname: null,
+      password: null
+    }
+  }
+  
+  handlegetName = (e) => {
+     e.preventDefault();
+     this.state.acountname = e.target.value;
+  }
+  handlePassword = (e) => {
+    e.preventDefault();
+    this.state.password = e.target.value;
+  }
+  handleSubmit = (e)=>{
+    e.preventDefault();
+    const {acountname, password} = this.state;
+    const {openid } = this.props;
+    if(!(acountname || password)){
+      Toast.fail('所填内容不能为空',1);
+      return;
+    }
+    if(!(/^1[3|4|5|8][0-9]\d{4,8}$/.test(password))){
+      Toast.fail('手机号格式不正确',1);
+      return;
+    }
+    const data  = {
+      'openid':'openid',
+      'name':acountname,
+      'mobile':password
+    }
+    axios.post('api/sign',data).then(res=>{
+      console.log(res,'提交表单');
+    }).catch(err=>{
+      console.log(err,'ddss')
+    })
+  }
   render() {
     return (
       <div className='checkinPage'>
@@ -24,19 +66,19 @@ export default class CheckSub extends React.Component {
           <div className='formname'>
             <label>*您的姓名</label>
             <div className='forminput'>
-              <input type='text'/>
+              <input type='text' onChange={this.handlegetName}/>
             </div>
           </div>
           <div className='formphone'>
             <label>*您的电话</label>
             <div className='forminput'>
-              <input type='text'/>
+              <input type='text' onChange={this.handlePassword}/>
             </div>
           </div>
         </div>
         <div style={{height:'9.23%'}}></div>
         <div className='submit'>
-          <img className='submitbtn' src={submit}></img>
+          <img className='submitbtn' src={submit} onClick={this.handleSubmit}></img>
         </div>
         <div style={{height:'1.2%'}}></div>
         <div className='gxline'>
