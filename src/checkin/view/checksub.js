da29134942d10b9374454c9efa866e35468ee9dd
sshch3612 <1758974924@qq.com>
2019-02-16 16:37:31 +0800

    fewf

--- src/checkin/view/checksub.js
+++ src/checkin/view/checksub.js
@@ -100,7 +100,7 @@ export default class CheckSub extends React.Component {
       //错误返回码
       const {response } = err;
       console.log(response,33);
-      if(response.status =='422' && response.data['openid'][0]=='微信用户 已经存在.'){
+      if(response.status =='422' && response.data.errors['openid'][0]=='微信用户 已经存在.'){
         const openid = localStorage.getItem('check_openId')
         axios.get(`/api/sign?openid=${openid}`)
           .then(res=>{
