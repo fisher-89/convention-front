78c1f9d44719ff06dc428f4493810c008deaa4b9
sshch3612 <1758974924@qq.com>
2019-02-17 13:23:44 +0800

    df

--- src/checkin/view/checkinView.js
+++ src/checkin/view/checkinView.js
@@ -22,7 +22,8 @@ export default class CheckIn extends React.Component {
       if(!code && !openId){
         const url = encodeURIComponent('http://cs.xigemall.com/checkin/index.html');//
         const appId = 'wx136539e52b4980bf';
-        window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`;
+        // window.history.replaceState(null,null,`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`)
+        window.location.replace(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`);
       }else if(code && !openId){
         const data = {'code':code}
         axios.post('/api/openid',
