3bc7ebe1b00f309fb046b0a1d063e7a077b45d53
sshch3612 <1758974924@qq.com>
2019-02-17 14:10:02 +0800

    fe

--- src/checkin/view/checkinView.js
+++ src/checkin/view/checkinView.js
@@ -24,8 +24,8 @@ export default class CheckIn extends React.Component {
         const url = encodeURIComponent('http://cs.xigemall.com/checkin/index.html');//
         const appId = 'wx136539e52b4980bf';
         // console.log(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`);
-        // window.location.replace(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`);
-        window.location.replace('https://zhidao.baidu.com/question/1963562072995994220.html');
+        window.location.replace(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`);
+        // window.location.replace('https://zhidao.baidu.com/question/1963562072995994220.html');
       }else if(code && !openId){
         const data = {'code':code}
         axios.post('/api/openid',
