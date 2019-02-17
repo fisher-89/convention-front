16a3f4890d42999f66e140eb10366ececa7d01a3
詹浩东 <1454443568@qq.com>
2019-02-17 16:42:07 +0800

    houtai

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Choice from './houtai/choice';
 
 
-ReactDom.render((
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/houtai" component={Choice} />
-    </Switch>
-  </BrowserRouter>
-), document.getElementById('root'));
+ReactDom.render(<Choice/>, document.getElementById('root'));
